3128ecb4883a4892c9c90e98a14bd04db1243962
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-29 13:29:26 +0100

    js expression that produces a value  commit

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,6 +1,12 @@
 function Message(){
+    const name = 'Divine';
+    if (name)
     return(
-        <h1>Hello world</h1>
+        <h1>Hello {name}</h1>
+
+    );
+    return(
+        <h1>Hello Peace</h1>
 
     );
 
